bd63ed6211abca7dbb69fe9c4727d983e17c4a11
Brian Larson <brian@brianlarson.com>
2024-11-01 15:50:32 -0500

    Finish ability to add employee rows with all data

--- script.js
+++ script.js
@@ -7,17 +7,31 @@ const annualSalaryInput = document.querySelector(`[data-testid="annualSalaryInpu
 
 // Create function to add employees to our list/table
 function addEmployee(event) {
+
   // Stop default button behavior where a button in a form refreshes the page
   event.preventDefault();
 
-  // Log the form input values
-  console.log(firstNameInput.value, lastNameInput.value, idInput.value, titleInput.value, annualSalaryInput.value);
+  // Get the tbody element so we can insert employee row elements (<tr>) into it
+  const tableBody = document.querySelector("#employeeList tbody");
+
+  // Insert the new employee row (<tr>) to our HTML table
+  tableBody.innerHTML += `
+    <tr>
+      <td>${firstNameInput.value}</td>
+      <td>${lastNameInput.value}</td>
+      <td>${idInput.value}</td>
+      <td>${titleInput.value}</td>
+      <td>${annualSalaryInput.value}</td>
+      <td><button>Delete</button></td>
+    </tr>
+  `;
 
   // Reset our add employee form so it's ready for the next addition
   resetForm();
+
 }
 
-// Add function to reset form inputs (quirk: cannot use vars set above for this DOM modification)
+// Add function to reset form inputs (quirk: cannot use vars set above for this DOM modification - why?)
 function resetForm() {
   document.querySelector(`[data-testid="firstNameInput"]`).value = '';
   document.querySelector(`[data-testid="lastNameInput"]`).value = '';
