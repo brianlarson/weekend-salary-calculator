627f819549e33d516b19fadcd410f166e35b0a62
Brian Larson <brian@brianlarson.com>
2024-10-31 23:49:36 -0500

    Create var for first input elem, wire up submit button to function that won't refresh page on click (prevent default behavior)

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the different inputs to access their values
+let firstNameInput = document.querySelector(`[data-testid="firstNameInput"]`);
+
+// Create function to add employees to our list/table
+function addEmployee(event) {
+  event.preventDefault();
+  console.log('addEmployee() called');
+}
