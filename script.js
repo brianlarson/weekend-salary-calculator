fa0eee9395614189db57a6a9d5bed171fe8a11a0
Brian Larson <brian@brianlarson.com>
2024-10-31 23:59:30 -0500

    Wire up first and last name inputs to be output to console on submission, add func to reset form inputs

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // Get the different inputs to access their values
 let firstNameInput = document.querySelector(`[data-testid="firstNameInput"]`);
+let lastNameInput = document.querySelector(`[data-testid="lastNameInput"]`);
 
 // Create function to add employees to our list/table
 function addEmployee(event) {
   event.preventDefault();
-  console.log('addEmployee() called');
+  console.log(firstNameInput.value, lastNameInput.value);
+  resetForm();
+}
+
+function resetForm() {
+  document.querySelector(`[data-testid="firstNameInput"]`).value = '';
+  document.querySelector(`[data-testid="lastNameInput"]`).value = '';
 }
